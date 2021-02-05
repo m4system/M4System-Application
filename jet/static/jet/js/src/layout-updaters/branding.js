a355f0c70cebd6cccedfa8d74faafd497cdad206
Daniel Gagnon <moi@danielgagnon.info>
2021-02-05 11:59:21 -0500

    fixed manage.py on centos 7 vanilla

---
+++ jet/static/jet/js/src/layout-updaters/branding.js
@@ -0,0 +1,23 @@
+var $ = require('jquery');
+
+var BrandingUpdater = function($branding) {
+    this.$branding = $branding;
+};
+
+BrandingUpdater.prototype = {
+    move: function($branding) {
+        $branding.detach().prependTo($('.sidebar-wrapper')).css('height', $branding.outerHeight());
+    },
+    run: function() {
+        var $branding = this.$branding;
+
+        try {
+            this.move($branding);
+        } catch (e) {
+            console.error(e, e.stack);
+        }
+
+        $branding.addClass('initialized');
+    }
+};
+
